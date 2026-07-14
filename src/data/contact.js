bc649a3bf5c2673d4c51cf800ccdff6cc7b91464
Monica <monicakhirani27@gmail.com>
2026-07-14 12:23:42 +0700

    #2nd Progress Landing Page Gressoy-Updating Texts dan Contact

--- src/data/contact.js
+++ src/data/contact.js
@@ -16,6 +16,8 @@ export const LOCATION = {
   hours: "08.00 – 22.00 WIB, setiap hari Senin-Sabtu",
   mapsUrl:
     "https://www.google.com/maps/search/?api=1&query=Gressoy+Jl.+Kolonel+Sugiono+No.34+Purwokerto",
+    embedSrc:
+    "https://www.google.com/maps?q=Gressoy,+Jl.+Kolonel+Sugiono+No.34,+Tipar,+Purwanegara,+Kec.+Purwokerto+Tim.,+Kabupaten+Banyumas,+Jawa+Tengah+53116&output=embed",
 };
 
 export const CONTACT = {
